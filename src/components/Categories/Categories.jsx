73352306a9356b00ebc636e92c23366d9e6f5d40
HAREESHRAVI <hareesh.unofficial@gmail.com>
2025-04-15 22:06:53 +0530

    fixed image bug

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,37 +1,37 @@
 import React from 'react';
 import './Categories.css';
-import gliomaImage from './assets/glioma.jpg';
-import meningiomaImage from './assets/meningioma.jpeg';
-import pituitaryImage from './assets/pituitary.jpg';
-import noTumorImage from './assets/no_tumor.jpg';
+import gliomaImage from '/src/assets/glioma.jpg';
+import meningiomaImage from '/src/assets/meningioma.jpeg';
+import pituitaryImage from '/src/assets/pituitary.jpg';
+import noTumorImage from '/src/assets/no_tumor.jpg';
 
 const Categories = () => {
   const cards = [
     {
       image: gliomaImage,
       title: "Glioma Tumor",
-      description: "Gliomas are a type of brain tumor...",
+      description: "Gliomas are a type of brain tumor that originate from glial cells, which support and protect neurons in the brain. They are among the most common and aggressive brain tumors, often growing rapidly and infiltrating surrounding brain tissue. Gliomas can impact vital brain functions depending on their location and severity.",
       button: "Read more",
       link: "https://www.hopkinsmedicine.org/health/conditions-and-diseases/gliomas",
     },
     {
       image: meningiomaImage,
       title: "Meningioma Tumor",
-      description: "Meningiomas are tumors that develop...",
+      description: "Meningiomas are tumors that develop from the meninges, the protective membranes covering the brain and spinal cord. Most meningiomas are non-cancerous and grow slowly, but their size and location can cause pressure on the brain, leading to symptoms like headaches, vision problems, or seizures.",
       button: "Read more",
       link: "https://www.hopkinsmedicine.org/health/conditions-and-diseases/meningioma",
     },
     {
       image: pituitaryImage,
       title: "Pituitary Tumor",
-      description: "Pituitary tumors form in the pituitary gland...",
+      description: "Pituitary tumors form in the pituitary gland, located at the base of the brain. These tumors can be benign (non-cancerous) or malignant (cancerous) and may affect hormone production, leading to various symptoms like fatigue, vision problems, and changes in growth or metabolism.",
       button: "Read more",
       link: "https://www.hopkinsmedicine.org/health/conditions-and-diseases/pituitary-tumors",
     },
     {
       image: noTumorImage,
       title: "No Tumor",
-      description: "No tumor refers to MRI scans...",
+      description: "No tumor refers to MRI scans where no abnormal growths or tumors are detected in the brain. These scans show a healthy brain with no signs of conditions like gliomas, meningiomas, or pituitary tumors, indicating no presence of any pathological changes",
       button: "Read more",
       link: "https://www.hopkinsmedicine.org/health/conditions-and-diseases/brain-tumor",
     },
